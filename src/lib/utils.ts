4a7fef48371f66523127d0977af181227d62875b
k1s1au1 <k1s1au1@gmail.com>
2026-09-15 07:57:08 +0300

    Restore family tree layout

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,15 +1,10 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-declare global {
-  var QuickActionsBanner: () => null;
-}
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 if (typeof window !== "undefined") {
   (window as any).cn = cn;
-  globalThis.QuickActionsBanner ??= () => null;
 }
